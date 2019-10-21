d845668a47f59dcec07c8a0ce5f61687bd366a8c
Justin Radomski <justin@justinradomski.me>
2019-10-21 00:59:54 -0400

    Fixed tabs

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,52 +9,52 @@ import "../components/layout.css"
 import mainStyles from "../styles/main.module.scss"
 
 const IndexPage = ({ data }) => (
-  <BackgroundImage
-      Tag="section"
-      fluid={data.backgroundImage.childImageSharp.fluid}
-      style={{
-          display: `flex`,
-          minHeight: `100vh`,
-          color: `white`,
-          position: `relative`,
-          overflow: `hidden`,
-          display: `flex`,
-      }}
-      className={mainStyles.main}>
-    <SEO title="No." />
-    <div
-        className={mainStyles.content}
+    <BackgroundImage
+        Tag="section"
+        fluid={data.backgroundImage.childImageSharp.fluid}
         style={{
-            flexBasis: `100%`,
-            textAlign: `center`
-        }}>
-        <h1
-            style={{
-                fontSize: 22,
-                fontWeight: `500`
-            }}>
-            Is Dan Quinn fired yet?
-        </h1>
-        <span
-            className="response"
-            style={{
-                fontSize: 100,
-                fontWeight: `700`,
-                display: `block`,
-                lineHeight: 1
-            }}>
-            No.
-        </span>
-    </div>
-    <div className={mainStyles.danImage}>
-        <Img
-            fluid={data.danImage.childImageSharp.fluid}
-            style={{
-                width: `100%`
-            }}
-            />
-    </div>
-  </BackgroundImage>
+            display: `flex`,
+            minHeight: `100vh`,
+            color: `white`,
+            position: `relative`,
+            overflow: `hidden`,
+            display: `flex`,
+        }}
+        className={mainStyles.main}>
+      <SEO title="No." />
+      <div
+          className={mainStyles.content}
+          style={{
+              flexBasis: `100%`,
+              textAlign: `center`
+          }}>
+          <h1
+              style={{
+                  fontSize: 22,
+                  fontWeight: `500`
+              }}>
+              Is Dan Quinn fired yet?
+          </h1>
+          <span
+              className="response"
+              style={{
+                  fontSize: 100,
+                  fontWeight: `700`,
+                  display: `block`,
+                  lineHeight: 1
+              }}>
+              No.
+          </span>
+      </div>
+      <div className={mainStyles.danImage}>
+          <Img
+              fluid={data.danImage.childImageSharp.fluid}
+              style={{
+                  width: `100%`
+              }}
+              />
+      </div>
+    </BackgroundImage>
 )
 
 export default IndexPage
