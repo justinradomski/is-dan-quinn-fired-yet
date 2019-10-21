c6e6c58f9fd8f7214e887dc13cd57bf6c6c0962a
Justin Radomski <justin@justinradomski.me>
2019-10-21 01:00:51 -0400

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,13 @@ module.exports = {
           }
       }
     },
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-142404881-2",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
